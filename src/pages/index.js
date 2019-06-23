ba9b296777a0e7360979304d0354c7dee83a9bb8
Kaizmaker <kaizmaker@gmail.com>
2019-06-23 08:10:53 +0800

    2019-6-image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>React design</h1>
+    <p>我是Kaiz，是UI/Ux設計師</p>
+    <p>好的設計等於良好的使用者體驗</p>
+    <Link to="/page-2/">參觀我的專案</Link>
   </div>
 )
 
